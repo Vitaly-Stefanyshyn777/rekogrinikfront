145fdc9407110c2aecdf9ec2dd4f2729954035ac
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-11-05 10:29:21 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -575,8 +575,14 @@ export default function RootLayout({
   return (
     <html lang="uk" dir="ltr">
       <head>
+        <link
+          rel="icon"
+          href="/vector-2.svg"
+          type="image/svg+xml"
+          sizes="any"
+        />
+        <link rel="shortcut icon" href="/vector-2.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/vector-2.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0a513d" />
